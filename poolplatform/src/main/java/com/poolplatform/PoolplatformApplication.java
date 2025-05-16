c15c125cc164a2dc16d55e62561a4b6e2ac930de
Humberto Daniel <gamekik92@gmail.com>
2025-05-15 23:25:34 -0600

    feat: :sparkles: upload backend files

---
+++ poolplatform/src/main/java/com/poolplatform/PoolplatformApplication.java
@@ -0,0 +1,13 @@
+package com.poolplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoolplatformApplication.class, args);
+	}
+
+}
