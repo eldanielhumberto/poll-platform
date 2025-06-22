a0032d590496780cf64412f2993957b0c8a87274
Humberto Daniel <gamekik92@gmail.com>
2025-06-22 17:33:43 -0600

    feat: :sparkles: Implementing OptionService functions

--- poolplatform/src/main/java/com/poolplatform/features/option/adapters/db/JpaOptionRepository.java
+++ poolplatform/src/main/java/com/poolplatform/features/option/adapters/db/JpaOptionRepository.java
@@ -2,37 +2,37 @@ package com.poolplatform.features.option.adapters.db;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.poolplatform.features.option.adapters.entities.OptionEntity;
+import com.poolplatform.features.option.adapters.mappers.OptionMapper;
 import com.poolplatform.features.option.domain.OptionRepository;
 import com.poolplatform.features.option.domain.models.Option;
 
+@Repository
 public interface JpaOptionRepository extends JpaRepository<OptionEntity, String>, OptionRepository {
 
     @Override
     default List<Option> get() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
+        return findAll().stream().map(OptionMapper::toOption).collect(Collectors.toList());
     }
 
     @Override
     default Optional<Option> get(String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
+        return findById(id).map(OptionMapper::toOption);
     }
 
     @Override
     default void remove(Option t) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+        delete(OptionMapper.toOptionEntity(t));
     }
 
     @Override
     default Option upsert(Option t) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'upsert'");
+        return OptionMapper.toOption(save(OptionMapper.toOptionEntity(t)));
     }
 
 }
