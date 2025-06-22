c4b0626dff6ad3edaf3664feefcd5873ea5d2605
Humberto Daniel <gamekik92@gmail.com>
2025-06-21 21:58:39 -0600

    refactor: :truck: /questions now returns the survey for each question

--- poolplatform/src/main/java/com/poolplatform/features/question/domain/models/QuestionSummary.java
+++ poolplatform/src/main/java/com/poolplatform/features/question/domain/models/QuestionSummary.java
@@ -3,10 +3,17 @@ package com.poolplatform.features.question.domain.models;
 public class QuestionSummary {
     private String id;
     private String questionText;
+    private SimpleSurvey survey;
 
     public QuestionSummary(Question question) {
         this.id = question.getId();
         this.questionText = question.getQuestionText();
+
+        SimpleSurvey survey = new SimpleSurvey();
+        survey.setId(question.getSurvey().getId());
+        survey.setTitle(question.getSurvey().getTitle());
+
+        this.survey = survey;
     }
 
     public String getId() {
@@ -23,5 +30,13 @@ public class QuestionSummary {
 
     public void setQuestionText(String questionText) {
         this.questionText = questionText;
+    }
+
+    public SimpleSurvey getSurvey() {
+        return survey;
+    }
+
+    public void setSurvey(SimpleSurvey survey) {
+        this.survey = survey;
     }
 }
---
+++ poolplatform/src/main/java/com/poolplatform/features/question/domain/models/SimpleSurvey.java
@@ -0,0 +1,22 @@
+package com.poolplatform.features.question.domain.models;
+
+public class SimpleSurvey {
+    private String id;
+    private String title;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
