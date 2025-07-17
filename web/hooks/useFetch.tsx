9a9aefecaf3815eccee27108420e6e761b59353d
Humberto Daniel <gamekik92@gmail.com>
2025-07-17 17:44:37 -0600

    fix: :bug: solving the problem of headers in useFetch

--- web/hooks/useFetch.tsx
+++ web/hooks/useFetch.tsx
@@ -1,8 +1,11 @@
 import useSWR, { SWRResponse } from 'swr';
 
 const fetcher = async (url: string, token: string | null) => {
+  console.log(token);
   const headers: HeadersInit = token ? { Authorization: token } : {};
-  const r = await fetch(url, headers);
+  const r = await fetch(url, { headers });
+  console.log(r.headers);
+
   const d = await r.json();
 
   return d.data;
